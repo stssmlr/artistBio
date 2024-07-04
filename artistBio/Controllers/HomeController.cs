2f59050c8604fa40907d87ca904629204a37ba67
stssmlr <stanislav.smolyar@gmail.com>
2024-07-04 23:45:59 +0300

    Add project files.

---
+++ artistBio/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+using artistBio.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace artistBio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult ArtistInfo()
+        {
+            return View();
+        }
+        public IActionResult LifeHistory()
+        {
+            return View();
+        }
+        public IActionResult Picture()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
